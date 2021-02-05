19d1abb1de356051d1746a623c941720debf24c4
etwasmitbaum <47484288+etwasmitbaum@users.noreply.github.com>
2021-02-05 15:25:44 +0100

    adjusted z homing points

--- Firmware/Marlin-2.0.7.2_Geetech_i3_ProB_Bowden_BLTouch_T8_Lead_Screw/Marlin/Configuration.h
+++ Firmware/Marlin-2.0.7.2_Geetech_i3_ProB_Bowden_BLTouch_T8_Lead_Screw/Marlin/Configuration.h
@@ -1400,8 +1400,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
-  #define Z_SAFE_HOMING_Y_POINT Y_CENTER  // Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT 10  // X point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT 2  // Y point for Z homing
 #endif
 
 // Homing speeds (mm/min)
