6abec809915ce1ac8ba11094cbf88d8eab7f8f98
etwasmitbaum <47484288+etwasmitbaum@users.noreply.github.com>
2021-02-16 15:27:24 +0100

    adjusted bltouch margin and offset

--- Firmware/Marlin-2.0.7.2_Geetech_i3_ProB_Bowden_BLTouch_T8_Lead_Screw/Marlin/Configuration.h
+++ Firmware/Marlin-2.0.7.2_Geetech_i3_ProB_Bowden_BLTouch_T8_Lead_Screw/Marlin/Configuration.h
@@ -994,15 +994,15 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -0.90 }
+#define NOZZLE_TO_PROBE_OFFSET { 0.5, 45.5, -0.90 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
-#define PROBING_MARGIN_LEFT 20
-#define PROBING_MARGIN_RIGHT 20
-#define PROBING_MARGIN_FRONT 20
-#define PROBING_MARGIN_BACK 20
+#define PROBING_MARGIN 0
+#define PROBING_MARGIN_LEFT 5
+#define PROBING_MARGIN_RIGHT 0
+#define PROBING_MARGIN_FRONT 5
+#define PROBING_MARGIN_BACK 5
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
